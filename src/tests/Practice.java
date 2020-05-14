9a82342d4f1fcbbe7985285b5c8ce30e1d96d33a
MCahitC <64571637+MCahitC@users.noreply.github.com>
2020-05-14 23:51:41 +0100

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,6 +3,8 @@ package tests;
 public class Practice {
 
     public static void main(String[] args) {
-        System.out.println("practice class");
+        System.out.println("practice class"); 
+         System.out.println("I added these changes via gitHub in my remote repo"); 
+        
     }
 }
